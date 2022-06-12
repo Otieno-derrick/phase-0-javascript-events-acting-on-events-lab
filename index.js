459eb3ab56406b42ff50ff9593c2f1c8bed6ba69
derrique <otieno.derrick@student.moringaschool.com>
2022-06-12 15:31:28 +0300

    commit tree 7c4d57ff63ac521201dd37925e5b6cf4cc3b5109

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+// Your code her
+// const dodger = document.getElementById("dodger");
+
+// dodger.style.bottom = "100px";
+// dodger.style.bottom = "0px";
+// dodger.style.left = "0px";
+
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
